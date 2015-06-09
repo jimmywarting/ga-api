756831c9da9652d6ac43db195c86cdc7e40c7062
Mikkel Bergmann <mikkel@jsguy.com>
2015-06-09 16:18:06 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # ga-api
-Google Analytics API access
+
+Google Analytics API access, with automated concurrency limiting and optional request caching.
+
+Features:
+
+* Concurrent requests limiting (GA allows 10 by default, and we retry on 403s)
+* Caching of requests (optional and configurable)
+* Loosely based on [this](https://www.npmjs.com/package/ga-analytics).
+
+<blockquote>
+	Note: you'll need to [setup a google service account](https://developers.google.com/identity/protocols/OAuth2ServiceAccount#creatinganaccount) in order to use this library, and have access to google analytics reporting.
+<blockquote>
+
+## Authenticating
+
+Before using the library, setup the [google service account](https://developers.google.com/identity/protocols/OAuth2ServiceAccount#creatinganaccount) as described in the link, then grab the following information:
+
+* clientId: "[CLIENTID].apps.googleusercontent.com"
+* email: "[CLIENTID]@developer.gserviceaccount.com" - you must add a [service email here](https://www.google.com/analytics/web/), for the correct view.
+* key: "[PATH/TO/PRIVATE.KEY.FILENAME].pem", as [described here](https://www.npmjs.com/package/ga-analytics)
+* ids: The view ID(s), eg: "ga:[VIEWID]"
+
+You can find the client ID and Email in the google developers console here (after creating it):
+
+![alt text](https://raw.githubusercontent.com/jsguy/ga-api/master/instructions/clientid.jpg "Client ID and email")
+
+And the view ID in the google analytics console here:
+
+![alt text](https://raw.githubusercontent.com/jsguy/ga-api/master/instructions/viewid.jpg "View ID")
+
+## Usage
+
+```javascript
+gaApi(args, callBack, settings)
+```
+
+Where:
+
+* args - all the authentication settings plus query parameters
+* callBack - a function that can take an `error` and a `result` argument
+* settings - an optional settings object where you can override the default settings
+
+
+### args
+
+* startDate - (YYYY-MM-DD), eg: 2015-05-21
+* endDate - (YYYY-MM-DD), eg: 2015-05-28
+* metrics - any metric to include, eg: "ga:session"
+* filters - any filtering you wnat, eg: "ga:pagePath=~/Home"
+* dimensions - any dimensions you like, eg: "ga:source"
+* maxResults - maximum number of results to return, eg: 100
+* sort - what to sort by, eg: "ga:source"
+
+The args must also include the Authentication information from above, ie: clientId, email, key and ids.
+
+
+### callBack
+
+This function receives an error object and the resulting data from your query, i.e:
+
+```javascript
+function(error, result) {
+    if(error) throw error;
+    //	Do something with result here
+}
+```
+
+
+### settings
+
+The settings can optionally override some deafult settings, these include:
+
+* cache - time in ms for caching requests, default is 0, ie: no cache
+* cacheDir - directory to save the cache - uses system temp dir by default (process.env.TMPDIR)
+* concurrentLimit - maximum concurrent requests - default is 10 (which is what Google set by default)
+* concurrentDelay - how long to delay if we get a 403 error, default is 1000ms
+* concurrentMaxRetry - how many times to retry after a 403 error - default is 3
+
+
+## Example
+
+```javascript
+var options = {...see above...},
+	gaApi = require('ga-api');
+
+gaApi(_.extend({}, options, {
+	startDate: "2015-05-21",
+	endDate: "2015-05-28",
+	dimensions: "ga:date",
+	metrics: "ga:pageviews"
+}), function(err, data) {
+	console.log(data);
+});
+```
+
+<blockquote>
+	Note: See [this page](https://developers.google.com/analytics/devguides/reporting/core/dimsmets#segments=true&cats=visitor,session,traffic_sources,adwords,goal_conversions,platform_or_device,geo_network,system,social_activities,page_tracking,internal_search,site_speed,app_tracking,event_tracking,ecommerce,social_interactions,user_timings,exceptions,content_experiments,custom_variables_or_columns,time,audience,adsense) for examples of dimensions and metrics
+</blockquote>
+
---
+++ index.js
@@ -0,0 +1,197 @@
+/*  Google analytics API acees with concurrency limiting and retry + caching built in.  */
+var googleapis  = require('googleapis'),
+    crypto = require('crypto'),
+    OAuth2      = googleapis.auth.OAuth2,
+    _ = require('lodash'),
+    fs = require('fs'),
+    compactObject = function(o) {
+        var clone = _.clone(o);
+        _.each(clone, function(v, k) {
+            if(!v) {
+                delete clone[k];
+            }
+        });
+        return clone;
+    },
+
+    //  For caching - generates a file name based on parameters
+    getCacheFileName = function(args){
+        //  Remove auth info
+        var fnArgs = _.clone(args),
+            shasum = crypto.createHash('sha1'),
+            fileName;
+        delete fnArgs.auth;
+        fileName = JSON.stringify(fnArgs);
+        shasum.update(fileName);
+        return cacheDir + shasum.digest('hex');
+    },
+
+    //  Execuates a ga query, or returns cache if available
+    gaExecuteQuery = function(args, callback, cache){
+        var retryCount = 0,
+            concurrentMaxRetry = 3;
+        concurrentUp();
+        googleapis.analytics('v3').data.ga.get(args, function(err, result) {
+            concurrentDown();
+            if(err) {
+                //  403 error: we probably just need to wait 1 sec...
+                if(err.code === 403) {
+                    setTimeout(function(){
+                        if(retryCount < concurrentMaxRetry) {
+                            retryCount += 1;
+                            gaExecuteQuery.apply(this, [args, callback, cache]);
+                        } else {
+                            //  Give up
+                            return callback(err);
+                        }
+                    }, concurrentDelay);
+                    return;
+                } else {
+                    return callback(err);
+                }
+            }
+            
+            //  Cache the response, if caching is on
+            if(cache ) {
+                var fileName = getCacheFileName(args);
+                fs.writeFileSync(fileName, JSON.stringify(result), {encoding: "utf8"});
+            }
+
+            callback(null, result);
+        });
+    },
+    //  Concurrency limiting, GA default is 10 concurrent connections
+    concurrentLimit = 10,
+    concurrentDelay = 1000,
+    concurrentMaxRetry = 3,
+    concurrentQueries = 0,
+    concurrentUp = function(){
+        concurrentQueries += 1;
+    },
+    concurrentDown = function(){
+        concurrentQueries -= 1;
+        //  Execute any queries
+        if(queryQueue.length > 0) {
+            gaExecuteQuery.apply(this, queryQueue.shift());
+        }
+    },
+    queryQueue = [],
+    gaQuery = function() {
+        if(concurrentQueries < concurrentLimit) {
+            gaExecuteQuery.apply(this, arguments);
+        } else {
+            var args = Array.prototype.slice.call(arguments);
+            queryQueue.push(args);
+        }
+    },
+
+    // Caching is off by default - to enable 15 mins caching: cache = 15 * 1000 * 60;
+    cache = 0,
+    cacheDir = process.env.TMPDIR;
+
+module.exports = function(args, callback, settings){
+    if(settings) {
+        cache = typeof settings.cache !== 'undefined'? settings.cache: cache;
+        cacheDir = typeof settings.cacheDir !== 'undefined'? settings.cacheDir: cacheDir;
+        concurrentLimit = typeof settings.concurrentLimit !== 'undefined'? settings.concurrentLimit: concurrentLimit;
+        concurrentDelay = typeof settings.concurrentDelay !== 'undefined'? settings.concurrentDelay: concurrentDelay;
+        concurrentMaxRetry = typeof settings.concurrentMaxRetry !== 'undefined'? settings.concurrentMaxRetry: concurrentMaxRetry;
+    }
+    
+    var jwt = new googleapis.auth.JWT(
+            args.email,
+            args.key,
+            null,
+            ['https://www.googleapis.com/auth/analytics.readonly']
+        ),
+        oauth2Client = new OAuth2(
+            args.clientId,
+            null,
+            'postmessage'
+        ),
+        sessionFile = cacheDir + "ga-runner-" + args.email.replace(/[^a-zA-Z\-]/gi, "_"),
+        authorize = function(authCallback) {
+            fs.exists(sessionFile, function(exists) {
+                if(exists) {
+                    fs.readFile(sessionFile, function(err, result) {
+                        //  If the file was read successfully
+                        if(!err) {
+                            //  If we cannot parse the file
+                            try {
+                                var json = JSON.parse(result);
+                                //  If session is still valid
+                                if(new Date(json.expiry_date) > Date.now()) {
+                                    return authCallback(null, json);
+                                }
+                            } catch(e) {}
+                        }
+                        concurrentUp();
+                        jwt.authorize(function(){
+                            concurrentDown();
+                            authCallback.apply(this, arguments);
+                        });
+                    });
+                } else {
+                    concurrentUp();
+                    jwt.authorize(function(err, result){
+                        concurrentDown();
+                        fs.writeFile(sessionFile, JSON.stringify(result));
+                        authCallback.apply(this, arguments);
+                    });
+                }
+                
+            });
+        };
+
+    //  Check we have required values
+    _.each(['ids', 'startDate', 'endDate', 'metrics'], function(value, key){
+        if(!args[value]) {
+            callback("Missing argument for", value);
+            return false;
+        }
+    })
+
+    //  Make sure we are authorized, then make request
+    authorize(function(err, result) {
+        if(err) {
+            callback(err);
+        } else {
+            oauth2Client.setCredentials({
+                access_token: result.access_token,
+                refresh_token: result.refresh_token
+            });
+
+            // https://developers.google.com/analytics/devguides/reporting/core/dimsmets
+            // https://developers.google.com/analytics/devguides/reporting/core/v3/coreDevguide
+            var gaArgs = compactObject({
+                "ids": args.ids,
+                "start-date": args.startDate,
+                "end-date": args.endDate,
+                "metrics": args.metrics,
+                "filters": args.filters,
+                'dimensions': args.dimensions,
+                "max-results": args.maxResults,
+                sort: args.sort,
+                auth: oauth2Client
+            });
+
+            //  Load the cached response, if caching is on
+            if(cache) {
+                var fileName = getCacheFileName(gaArgs),
+                    stats;
+
+                fs.readFile(fileName, "utf8", function(err, data){
+                    if(!err) {
+                        stats = fs.statSync(fileName);
+                        if(stats.isFile() && (stats.birthtime >= ((new Date()).getTime() - cache))) {
+                            return callback(null, JSON.parse(data));
+                        }
+                    }
+                    gaQuery(gaArgs, callback, cache);
+                });
+            } else {
+                gaQuery(gaArgs, callback);
+            }
+        }
+    });
+};
